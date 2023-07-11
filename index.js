6d7e7a3fb1dbeb8b964391cd6e1549af79835a96
manish.ranjan <manish.ranjan@celestialsys.com>
2023-07-11 10:44:50 +0530

    loginToken route

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const cookieparser = require('cookie-parser');
-const jwt = require('jsonwebtoken')
-const bodyParser = require('body-parser');
-  
+import dotenv from 'dotenv';
+import express from 'express';
+import cookieparser from 'cookie-parser';
+import fetch from 'node-fetch';
+import cors from "cors";  
 // Configuring dotenv
 dotenv.config();
   
@@ -13,15 +12,105 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser());
-  
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true
+    })
+  );
+const refresh_url="http://14.97.142.161:8080/api/refresh-tokens";
+const access_url="http://14.97.142.161:8080/api/access-tokens";
+const login_url="http://14.97.142.161:8080/api/login-tokens";
+
+async function getRefreshToken(){        
+    let ts = new Date().getTime();
+    let data = {
+      userName: "dg@celestialsys.com",
+      password: "test",
+      clientOrgRef: "Data Governance"
+     }
+   const refreshResponse = await fetch(refresh_url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `YELLOWFIN ts=${ts}, nonce=123`,
+        'Accept': 'application/vnd.yellowfin.api-v2+json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+      },
+    });
+  const refreshData = await refreshResponse.json();
+  const refreshToken = refreshData.securityToken;
+  return refreshToken 
+}
+
+async function getAccessToken(){        
+    let ts = new Date().getTime();
+    let data = {
+      userName: "dg@celestialsys.com",
+      password: "test",
+      clientOrgRef: "Data Governance"
+     }
+   let refreshToken = await getRefreshToken();
+   const accessResponse = await fetch(access_url, {
+   method: "POST",
+   body: JSON.stringify(data),
+   headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `YELLOWFIN ts=${ts}, nonce=123, token=${refreshToken}`,
+    'Accept': 'application/vnd.yellowfin.api-v2+json',
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    },
+    });
+    const accessData = await accessResponse.json();
+    const accessToken = accessData.securityToken;
+    return accessToken; 
+    }   
+
+    async function getLoginToken(){        
+        let ts = new Date().getTime();
+        let data = {
+          userName: "dg@celestialsys.com",
+          password: "test",
+          clientOrgRef: "Data Governance"
+         }
+       let accessToken = await getAccessToken();
+       const loginResponse = await fetch(login_url, {
+       method: "POST",
+       body: JSON.stringify(data),
+       headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `YELLOWFIN ts=${ts}, nonce=123, token=${accessToken}`,
+        'Accept': 'application/vnd.yellowfin.api-v2+json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+        },
+        });
+        const loginData = await loginResponse.json();
+        const loginToken = loginData.securityToken;
+        return loginToken; 
+        }  
+      
 const userCredentials = {
     username: 'admin',
     password: 'test',
     email: 'dg@celestialsys.com'
 }
+const Url = 'http://34.236.87.39:8080'; 
 
-const Url = 'http://34.236.87.39:8080';
-  
+app.get('/loginToken', async (req,res)=>{
+    try{
+        const loginToken = await getLoginToken();
+        return  res.status(200).send({loginToken: loginToken});
+    }
+    catch(error){
+        return res.status(404).send({message:+ error });
+    }
+})
+
+
 app.post('/login', async (req, res) => {
     // Destructuring username & password from body
     const { email, password } = req.body;
@@ -30,89 +119,11 @@ app.post('/login', async (req, res) => {
     if (email === userCredentials.email && 
         password === userCredentials.password) {
         return res.status(200).json({ message: 'Login Successful', data: userCredentials});
-        //  try{
-        //     let _data = {
-        //         "userName": email,
-        //         "password": password,
-        //         "clientOrgRef": "Data Governance"
-        //        }
-        //       let ts = new Date().getTime();    
-        //       const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        //             method: "POST",
-        //             body: JSON.stringify(_data),
-        //             headers: {
-        //             "Content-Type": "application/json",
-        //             "Authorization": `YELLOWFIN ts=${ts}, nonce=123`,
-        //             "Accept" : "application/vnd.yellowfin.api-v2+json"  
-        //            }
-        //           })
-                 
-        //         const data = await response.json();
-
-        //         //   .then(response => response.json()) 
-        //         //   .then(json => console.log(json));
-        //         //   .catch(err => console.log(err)); 
-        //  } catch(error) {
-        //     console.log("error in fetching refresh token", error);
-        //  }   
-           
-        //creating a access token
-        // const accessToken = jwt.sign({
-        //     username: userCredentials.username,
-        //     email: userCredentials.email
-        // }, process.env.ACCESS_TOKEN_SECRET, {
-        //     expiresIn: '10m'
-        // });
-        // Creating refresh token not that expiry of refresh 
-        //token is greater than the access token
-          
-        // const refreshToken = jwt.sign({
-        //     username: userCredentials.username,
-        // }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
-  
-        // Assigning refresh token in http-only cookie 
-        // res.cookie('jwt', refreshToken, { httpOnly: true, 
-        //     sameSite: 'None', secure: true, 
-        //     maxAge: 24 * 60 * 60 * 1000 });
-        // return res.json({ accessToken });
     }
     else {
         return res.status(404).json({ message: 'Invalid Credentials' });
-        // Return unauthorized error if credentials don't match
-        // return res.status(406).json({ 
-        //     message: 'Invalid credentials' });
     }
 });
-  
-// app.post('/refresh', (req, res) => {
-//     if (req.cookies?.jwt) {
-  
-//         // Destructuring refreshToken from cookie
-//         const refreshToken = req.cookies.jwt;
-  
-//         // Verifying refresh token
-//         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, 
-//         (err, decoded) => {
-//             if (err) {
-  
-//                 // Wrong Refesh Token
-//                 return res.status(406).json({ message: 'Unauthorized' });
-//             }
-//             else {
-//                 // Correct token we send a new access token
-//                 const accessToken = jwt.sign({
-//                     username: userCredentials.username,
-//                     email: userCredentials.email
-//                 }, process.env.ACCESS_TOKEN_SECRET, {
-//                     expiresIn: '10m'
-//                 });
-//                 return res.json({ accessToken });
-//             }
-//         })
-//     } else {
-//         return res.status(406).json({ message: 'Unauthorized' });
-//     }
-// })
   
 app.listen(process.env.PORT, () => {
     console.log(`Server active on http://34.236.87.39:${process.env.PORT}!`);
